90d6f6be499a7b8969007ce2a5c6102b20e88b02
david11yf29 <david11yf29@gmail.com>
2020-12-24 12:58:04 +0800

    updated

--- server.js
+++ server.js
@@ -2,9 +2,35 @@ const express = require('express');
 
 const app = express();
 
+const database = [
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+];
+
+
 app.get('/', (req, res) => {
   res.send('this is working');
-})
+});
+
+app.post('/signin', (req, res) => {
+  res.send('signing')
+});
 
 app.listen(3000, () => {
   console.log('App is running on port 3000');
