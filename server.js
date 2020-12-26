cd3a8e58ddc1c60a40940c353e7f1b0abf436373
david11yf29 <david11yf29@gmail.com>
2020-12-26 11:35:54 +0800

    updated

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex')
 
-knex({
+const postgres = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'your_database_user',
-    password : 'your_database_password',
-    database : 'myapp_test'
+    user : 'DoDoTea',
+    password : '',
+    database : 'smart-brain'
   }
 });
+
+postgres.select('*').from('users');
 
 const app = express();
 
